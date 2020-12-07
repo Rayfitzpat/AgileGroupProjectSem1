79cf9cb4765b0714cc855cf104e7a29b197c6fb1
Yuliia Dovbak <yulia.dovbak@gmail.com>
2020-12-07 13:01:42 +0000

    Trying to see branch tree

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
 public class Test{
     public static void main(String[] args) {
         System.out.println("Test commit no. 1");
+
+        System.out.println("Testing branch tree visuals");
     }
+
+
 }
