e00c50ef137bfbfa1c9e9b8e8e5f0085a3097a63
Yuliia Dovbak <yulia.dovbak@gmail.com>
2020-12-07 13:21:46 +0000

    Testing Customer3

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,12 @@
 public class Test{
     public static void main(String[] args) {
+        int i = 2;
+        int j = 3;
         System.out.println("Test commit no. 1");
 
         System.out.println("Testing branch tree visuals...");
 
-        int number1 = 2;
-        int number2 = 3;
+
     }
 
 
